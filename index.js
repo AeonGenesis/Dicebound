e1e9df66f94dc2b70e01eea4b8dc446a2add4142
AeonGenesis <exveras@hotmail.com>
2021-04-06 06:42:17 -0400

    changed it back to set activity and set the type

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ client.login(process.env.DISCORD_TOKEN);
 
 client.on('ready', () => {
     console.log(`${client.user.tag} has logged in.`);
-    client.user.setStatus("Listening for #help");
+    client.user.setActivity("#roll", { type: "LISTENING" });
 })
 
 
