8731050d0f455bb99c0e0c28dfe41b9fc1754e8d
AeonGenesis <exveras@hotmail.com>
2021-04-07 00:55:37 -0400

    if statement

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ client.on('message', (message) => {
     const rollsOutput = roll.rolls.map(result => {
        return result.focused ? '**' + result.value + '**': result.value;
     });
-    if (roll.totalSuccesses >= roll.neededSuccesses){
+    if (roll.rolls.totalSuccesses >= roll.rolls.neededSuccesses){
         message.channel.send(`Success! ${rollsOutput} with ${roll.totalSuccesses} successes using ${roll.rolls.map(usedFocus => usedFocus.focused).reduce((a, b) => a + b)} focus`);
     } else {
         message.channel.send(`Failed! ${rollsOutput} with ${roll.totalSuccesses} successes using ${roll.rolls.map(usedFocus => usedFocus.focused).reduce((a, b) => a + b)} focus`);
